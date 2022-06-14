586ce044b31027cec4286f0098a6f7eaa978c761
Griffin <gmd298@gmail.com>
2022-06-14 08:34:28 -0400

    commit tree 90585b699f89a8885c8f2f0df705791a54cd2746

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
 class Author < ApplicationRecord
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { is: 10 }
+
+
+  def has_name
+
+  end
+
 end
